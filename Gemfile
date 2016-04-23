f22149f9e8bb18f620bdb5a3a0fa6c481be5df78
itsironhawk <itsironhawk@gmail.com>
2016-04-23 11:46:40 -0400

    init commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem "espeak-ruby", require: "espeak"
+gem "mplayer", require: "mplayer" , :git => "git://github.com/CodeMonkeySteve/ruby-mplayer"
---
+++ art.rb
@@ -0,0 +1,80 @@
+require 'mplayer-ruby'
+$row = ["f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, "f".bg_blue.blue, ]
+$totalSong = ""
+
+class String
+  def bg_black;       "\e[40m#{self}\e[0m" end
+  def bg_red;         "\e[41m#{self}\e[0m" end
+  def bg_green;       "\e[42m#{self}\e[0m" end
+  def bg_brown;       "\e[43m#{self}\e[0m" end
+  def bg_blue;        "\e[44m#{self}\e[0m" end
+  def bg_magenta;     "\e[45m#{self}\e[0m" end
+  def bg_cyan;        "\e[46m#{self}\e[0m" end
+  def bg_gray;        "\e[47m#{self}\e[0m" end
+
+  def black;          "\e[30m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
+end
+
+def generate()
+  columnChangeVal = rand(0..80)
+  columnChangeVal.times do
+    columnToChange = rand(0..80)
+    color = rand(0..4)
+    case color
+    when 0 then $row[columnToChange] = "a".bg_blue.blue(); $totalSong = $totalSong + "a"
+    when 1 then $row[columnToChange] = "b".bg_magenta.magenta(); $totalSong = $totalSong + "b"
+    when 2 then $row[columnToChange] = "c".bg_cyan.cyan(); $totalSong = $totalSong + "c"
+    when 3 then $row[columnToChange] = "d".bg_green.green(); $totalSong = $totalSong + "d"
+    when 4 then $row[columnToChange] = "e".bg_brown.brown(); $totalSong = $totalSong + "e"
+    end
+  end
+  puts $row.join("").to_s
+end
+
+30.times {generate()}
+
+File.open("art.txt", "w") do |file|
+  file.write($totalSong)
+end
+
+@sleepTime = 0.2
+def play(instrument)
+  mp = MPlayer::Slave.new "#{instrument}.mp3"
+  @sleepTime = (mp.get :time_length).to_s.to_f - 0.05
+end
+
+inst = "drum1"
+inst2 = "drum2"
+
+$totalSong.split("").each do |s|
+  sleep @sleepTime
+  case s
+    when "a"
+      play(inst)
+    when "b"
+      play(inst2)
+    when "c"
+      play(inst)
+      sleep @sleepTime
+      play(inst)
+      sleep @sleepTime
+      play(inst2)
+    when "e"
+      play(inst2)
+      sleep @sleepTime
+      play(inst)
+      sleep @sleepTime
+      play(inst2)
+    when "f"
+      play(inst2)
+      sleep @sleepTime
+      play(inst2)
+  end
+end
